4425d9755d503e6de79e12111d79c41ee406630d
cincy sebastian <cincy123@gmail.com>
2024-02-05 13:43:53 -0700

    changing println to print

--- InteractiveGreeter.java
+++ InteractiveGreeter.java
@@ -6,30 +6,30 @@ public class InteractiveGreeter {
         System.out.println("Interactive Greeter Project");
         //Creating Scanner for reading name
         Scanner yourName = new Scanner(System.in);
-        System.out.println("What is your name?:");
+        System.out.print("What is your name?:");
         //assigning the name value to a variable;
         String name = yourName.nextLine();
         //Creating Scanner for reading place
         Scanner yourPlace = new Scanner(System.in);
-        System.out.println("Where are you from?:");
+        System.out.print("Where are you from?:");
         //assigning the name value to a variable;
         String place = yourPlace.nextLine();
         //Creating Scanner for reading age
         Scanner yourAge = new Scanner(System.in);
-        System.out.println("What your Age?:");
+        System.out.print("What your Age?:");
         //assigning the name value to a variable;
         int age = yourAge.nextInt();
         //Printing the output using printf
         System.out.printf("Hello %s from %s. You are %d years old.\n", name, place, age);
         //new scanner for reading activity
         Scanner activity = new Scanner(System.in);
-        System.out.println("What's something you like to do?:");
+        System.out.print("What's something you like to do?:");
         //assigning the activity to a variable;
         String favouriteActivity = activity.nextLine();
         System.out.printf("Have fun next time you play %s !\n", favouriteActivity);
         //new scanner for reading donation amount
         Scanner donation = new Scanner(System.in);
-        System.out.println("How much would you like to donate today?:");
+        System.out.print("How much would you like to donate today?:");
         //assigning the donation to a variable;
         double donationAmount = donation.nextDouble();
         System.out.printf("You will donate $ %.2f today. Yay!", donationAmount);
